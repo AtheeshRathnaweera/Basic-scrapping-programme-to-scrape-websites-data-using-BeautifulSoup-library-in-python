7d8c62de6e2da81369ce30476cfede2f64f36527
AtheeshRathnaweera27 <rathnaweeraatheesh72@gmail.com>
2019-05-25 13:25:29 +0530

    Further dev

--- scraper.py
+++ scraper.py
@@ -18,10 +18,32 @@ soup = BeautifulSoup(page, "html.parser")
 date_value = soup.find('h2', attrs={'class':'date-header'}) #Date of the post
 main_header_value = soup.find('h3', attrs={'class': 'post-title entry-title'}) #main header title
 
+post_body = soup.find('div',attrs={'id': 'post-body-1322918833010608425'})
+
  # strip() is used to remove starting and trailing
 date_string = date_value.text.strip()
 main_header_string = main_header_value.text.strip()
 
-print ("Date of the post "+ date_string)
-print ("Received data: "+ main_header_string)
+#Getting the body parts 
+
+soupTwo = BeautifulSoup('<span style="font-size: large;">What is a CV?</span>',  'html.parser')
+titleOne= soupTwo.span.text.strip()#First Subtitle
+
+soupThree = BeautifulSoup('<span style="font-size: large;">What to include in a CV ?</span>',  'html.parser')
+titleSec= soupThree.span.text.strip()#Second Subtitle
+
+soupFour = BeautifulSoup('<span style="font-size: large;">What not to include ?</span>',  'html.parser')
+titleThird= soupFour.span.text.strip()#Third Subtitle
+
+soupFive= BeautifulSoup('<span style="font-size: large;">An important fact</span>',  'html.parser')
+titleFour= soupFive.span.text.strip()#Fourth Subtitle
+
+print ("\nDate of the post "+ date_string)
+print ("\nPost name : "+ main_header_string)
+
+print ("\n "+ titleOne)
+
+print (" "+ titleSec)
+print (" "+ titleThird)
+print (" "+ titleFour)
 
