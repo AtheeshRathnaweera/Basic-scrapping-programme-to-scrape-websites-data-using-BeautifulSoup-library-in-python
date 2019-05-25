cb90412f23aaccaa3581c88973a7d618a58b8a73
AtheeshRathnaweera27 <rathnaweeraatheesh72@gmail.com>
2019-05-26 00:20:59 +0530

    Final commit

--- scraper.py
+++ scraper.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 # url of the site
 quote_page = "https://se15053.blogspot.com/2018/12/how-to-write-cv.html"
 
+
 # query the website and return the html to the variable ‘page’
 page = urlopen(quote_page)
 
@@ -18,7 +19,7 @@ soup = BeautifulSoup(page, "html.parser")
 date_value = soup.find('h2', attrs={'class':'date-header'}) #Date of the post
 main_header_value = soup.find('h3', attrs={'class': 'post-title entry-title'}) #main header title
 
-post_body = soup.find('div',attrs={'id': 'post-body-1322918833010608425'})
+
 
  # strip() is used to remove starting and trailing
 date_string = date_value.text.strip()
@@ -29,6 +30,9 @@ main_header_string = main_header_value.text.strip()
 soupTwo = BeautifulSoup('<span style="font-size: large;">What is a CV?</span>',  'html.parser')
 titleOne= soupTwo.span.text.strip()#First Subtitle
 
+post_first_para_top = soup.find('blockquote',attrs={'class': 'tr_bq'})
+One_para_quote = post_first_para_top.text.strip()
+
 soupThree = BeautifulSoup('<span style="font-size: large;">What to include in a CV ?</span>',  'html.parser')
 titleSec= soupThree.span.text.strip()#Second Subtitle
 
@@ -38,12 +42,21 @@ titleThird= soupFour.span.text.strip()#Third Subtitle
 soupFive= BeautifulSoup('<span style="font-size: large;">An important fact</span>',  'html.parser')
 titleFour= soupFive.span.text.strip()#Fourth Subtitle
 
-print ("\nDate of the post "+ date_string)
+
+#Whole article at once
+One_para_inne = soup.find('div',attrs={'id':'post-body-1322918833010608425' })
+test_text = One_para_inne.text.strip()
+
+print ("\nDate of the post : "+ date_string)
 print ("\nPost name : "+ main_header_string)
 
 print ("\n "+ titleOne)
-
+print ("\t "+ One_para_quote)
 print (" "+ titleSec)
 print (" "+ titleThird)
 print (" "+ titleFour)
+
+
+print("\n\n\t\t\t\t\t*******************Whole article at once***********************")
+print ("\n "+ test_text)
 
